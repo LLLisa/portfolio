3fa4bcf7facba755aaf54dd26b2c36ced966fad7
Lisa Knox <lisaknox83@gmail.com>
2023-04-01 11:43:31 -0400

    fix scrip

--- build/server/api/index.js
+++ build/server/api/index.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
+const example_1 = __importDefault(require("./example"));
 const app = (0, express_1.default)();
 app.use('/public', express_1.default.static(path_1.default.join(__dirname, '../../../public')));
 app.use('/dist', express_1.default.static(path_1.default.join(__dirname, '../../../dist')));
+app.use('/example', example_1.default);
 app.get('/', (req, res, next) => {
     try {
         res.sendFile(path_1.default.join(__dirname, '../../../public/index.html'));
