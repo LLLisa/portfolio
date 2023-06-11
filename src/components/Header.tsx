efd3c4f182c0d8cf551c2114332dfe92de7f90c8
Lisa Knox <lisaknox83@gmail.com>
2023-06-11 16:15:05 -0400

    navbar shows current page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,25 @@ import { Link, useParams } from 'react-router-dom';
 export default () => {
   const page = useParams().page as string;
 
-  console.log(page);
+  const getWhichSelected = (thisLink: string): string => {
+    // console.log(thisLink, page);
+    if (thisLink === page) return 'currentPageLink';
+    return '';
+  };
 
   return (
     <header>
       <h1>Header</h1>
       <nav>
-        <Link to="/">about</Link>
-        <Link to="/inventory">inventory</Link>
-        <Link to="/rgs">rgs</Link>
+        <Link to="/" className={getWhichSelected('about')}>
+          about
+        </Link>
+        <Link to="/inventory" className={getWhichSelected('inventory')}>
+          inventory
+        </Link>
+        <Link to="/rgs" className={getWhichSelected('rgs')}>
+          rgs
+        </Link>
       </nav>
     </header>
   );
