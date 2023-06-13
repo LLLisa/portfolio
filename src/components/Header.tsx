6be4231e43a0afaffa36381a80d536d9f7706ea8
Lisa Knox <lisaknox83@gmail.com>
2023-06-13 15:28:38 -0400

    tweaked start script

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,25 +3,25 @@ import { Link, useParams } from 'react-router-dom';
 import { routesArray } from '../utils';
 
 export default () => {
-  const page = useParams().page as string;
+    const page = useParams().page as string;
 
-  const getWhichSelected = (linkName: string): string => {
-    if (linkName === page) return 'currentPageLink';
-    return '';
-  };
+    const getWhichSelected = (linkName: string): string => {
+        if (linkName === page) return 'currentPageLink';
+        return '';
+    };
 
-  return (
-    <header>
-      <h1>Header</h1>
-      <nav>
-        {routesArray.map((route) => {
-          return (
-            <Link key={route} to={route} id={getWhichSelected(route.slice(1))}>
-              {route.slice(1)}
-            </Link>
-          );
-        })}
-      </nav>
-    </header>
-  );
+    return (
+        <header>
+            <h1>Lisa Anne Knox </h1>
+            <nav>
+                {routesArray.map((route) => {
+                    return (
+                        <Link key={route} to={route} id={getWhichSelected(route.slice(1))}>
+                            {route.slice(1)}
+                        </Link>
+                    );
+                })}
+            </nav>
+        </header>
+    );
 };
