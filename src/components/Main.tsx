5bdf5bda967ae1a57a95852146dccd0a5bd475b4
Lisa Knox <lisaknox83@gmail.com>
2023-06-16 16:29:45 -0400

    Heroku

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,18 +6,19 @@ import { generatePage, TPageData, about, inventory, rgs } from './views';
 export default () => {
     const page = useParams().page as string;
 
-    const pageConst: Record<string, TPageData> = {
-        about: about,
-        inventory: inventory,
-        rgs: rgs,
-    };
+    // const pageConst: Record<string, TPageData> = {
+    //     about: about,
+    //     inventory: inventory,
+    //     rgs: rgs,
+    // };
 
     return (
         <main>
             <Link className='directionalNav navLeft' to={'/' + routeSelector(page, -1)}>
                 &lt;
             </Link>
-            <article>{generatePage(pageConst[page])}</article>
+            <article>{generatePage(about)}</article>
+            {/* <article>{generatePage(pageConst[page])}</article> */}
             <Link className='directionalNav navRight' to={routeSelector(page, 1)}>
                 &gt;
             </Link>
