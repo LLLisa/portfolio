367ecd1a6907f9ba91ea47971a6bcd8bf538dc95
Lisa Knox <lisaknox83@gmail.com>
2023-06-16 16:07:39 -0400

    Heroku

--- src/components/views/generatePage.tsx
+++ src/components/views/generatePage.tsx
@@ -8,10 +8,12 @@ export type TPageData = {
 export default (pageData: TPageData) => {
     const { heading, content } = pageData;
 
-    return (
+    return pageData ? (
         <section>
             {heading && <h2>{heading}</h2>}
             {content && <p>{content}</p>}
         </section>
+    ) : (
+        <>loading...</>
     );
 };
